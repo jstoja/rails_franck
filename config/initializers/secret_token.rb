e07bbf5490cbc34bbb39c30cb8b732301fb9e870
Julien Bordellier <julienbordellier@gmail.com>
2013-08-10 19:02:49 +0200

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FranckTest::Application.config.secret_key_base = 'a4850842be6f54faf6fd2343d63d5f0ff5825b202a8bc097d721ad2b7cd33e360651b36d3d3aa8248b4e32a0aba0ffae25dc8bf0f3fff03497ac8f46d342d1f9'
